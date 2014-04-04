023c8a853a79be67fa6e0634224b344d3adc1234
drandreasdr <andreas.draganis@gmail.com>
2014-04-04 14:55:04 +0200

    Added methods to construct nonogram etc.

--- nonogram.py
+++ nonogram.py
@@ -1,50 +1,115 @@
+import copy
+
 class Nonogram:
     """
     Stores:
-    rows, cols: both lists of Line objects
+    rowscols: a list containing two lists of Line objects, representing rows and cols, respectively.
+    Contains rules and can also describe the configuration of the Nonogram.
+    configuration: 2D list containing color indices, representing the (partial or
+    complete) configuration of the Nonogram. Separate from the configuration (used by the solver) as described by
+    the state of the Line objects.
     colormap: list of hexadecimal color codes. 0:th element empty ?TODO?
+    Note: cells are identified by a number: -2: unknown, -1: known and empty,
+    >= 0: colors as described by "colormap"
     """
-    def __init__(self, ):
-        1
+    def __init__(self, nrows, ncols):
+        """
+        Just initializes row and col lists. Other methods take care of setting them up.
+        """
+        self.nrowscols = (nrows, ncols)
+        self.rowscols = [[],[]]
+        self.issetupcomplete = False
 
-    def hasvalidcombination_recursive_operative(self, i_line):
+    def setup(self):
+        #Open text file, read color indices and set up, find ROWS line, define rows, find COLS line, define cols.
+        pass
+
+    def setcolorindices(self, colors):
         """
-        Returns true if given line has a combination that is valid, and for which the next line (if it exists) returns True when this same method is called upon it
-        Note: Should only be called by hassolution_operative and by itself
+        colors: a list of hexadecimal color codes
         """
-        moremovesavailable = False
-        while moremovesavailable:
-            if nonogramsolvee.line[i_line].hasmorecombinations():
-                nonogramsolvee.line[i_line].setnextcombination()
+        self.colormap = colors
+
+    def addline(self, dim, seg_nblocks, seg_coloridx):
+        """
+        Adds a Line consisting of segments of quantity and color defined by the input parameters
+        dim: dimension: 0 for row, 1 for col
+        seg_nblocks: list of quantities of added segments
+        seg_color: list of color indices of added segments
+        Note: the method Segment.appendsegment() will warn if the line is overcrowded.
+        """
+        assert len(self.rowscols[dim]) <= self.nrowscols[dim], "No more lines can be added to this dimension"
+        self.rowscols[dim].append(self.Line(self.nrowscols[dim], seg_nblocks, seg_coloridx))
+
+        self.setissetupcomplete()
+
+    def setissetupcomplete(self):
+        """
+        Checks if setup of nonogram is complete (i.e. all rows and cols have been defined)
+        """
+        issetupcomplete = True
+        for i in range(len(self.rowscols)):
+            if len(self.rowscols[i]) != self.nrowscols[i]:
+                issetupcomplete = False
+        self.issetupcomplete = issetupcomplete
+
+    def hassolution_operative(self):
+        """
+        Returns true if given
+        """
+        #Find
+        soldim = 0 #0 is default
+        hasnextvalidconfiguration_recursive_operative(0, soldim)
+        #Do it again to see if there are more than one solution
+
+
+    def hasnextvalidconfiguration_recursive_operative(self, i_line):
+        """
+        Returns true if the line sollines[i_line] can be advanced (via setnextcombination()) to
+        a configuration that is valid, and for which the next line (if it exists)
+        returns True when this same method is called upon it (thereby "recursive").
+        "operative": operates on sollines and sollinesperp (administered by
+        hassolution_operative(), see description there). Note that if a solution is found,
+        self.rowscols[soldim] will contain the corresponding configuration (note that there
+        might be multiple valid solutions).
+        Note: should only be called by hassolution_operative and by itself
+        """
+        perpdim = 1 - soldim
+        while True:
+            if self.rowscols[soldim][i_line].hasmorecombinations():
+                self.rowscols[soldim][i_line].setnextcombination()
                 if isboardvalid():
                     if i_line == n_line-1: #at last line
                         return True
                     if hasvalidcombination_recursive_operative(i_line+1): #recursive call
                         return True
-                    #TODO try to combine the above two as soon as everything works, make sure it short cicuits
+                    #TODO try to combine the above two as soon as everything works, make sure it short circuits
             else: #if no more combinations
-                moremovesavailable = False
-        #if still here, we have failed to find a valid combination
-        return False
+                return False
 
-    def hassolution_operative(self):
-        hasvalidcombination_recursive_operative(self, 0)
 
     class Line:
-        """Represents a row or a column in the nonogram. It stores a
-        list of Segments
+        """
+        Represents a row or a column in the nonogram. It stores a list of Segments.
+        The Lines of the Nonogram are used to store the rules defining it. It can
+        also optionally be used to store the configuration of the nonogram (a
+        functionality which is used by the solver), although this configuration
+        is separate from the one stored in "configuration" (which is mostly used in relation
+        to the "player" interface).
         Stores:
         nblocks: number of blocks in the Line
         segments: a list of Segments.
         """
-        def __init__(self, nblocks, ):
+        def __init__(self, nblocks, seg_nblocks, seg_coloridx):
             self.nblocks = nblocks
             self.segments = []
             self.clearpositions()
+            for i in range(len(seg_nblocks)):
+                self.appendsegment(seg_nblocks[i], seg_coloridx[i])
 
         def clearpositions(self):
-            for seg in segments:
-                self.seg_positions[i] = -1;
+            for seg in self.segments:
+                seg.pos = -1
 
         def appendsegment(self, nblocks, coloridx):
             """
@@ -70,7 +135,7 @@ class Nonogram:
 
         def gettotalsegmentlength(self):
             length = 0
-            for seg in segments:
+            for seg in self.segments:
                 length += seg.getfulllength()
             return length
 
@@ -91,11 +156,11 @@ class Nonogram:
                 self.emptytail = False
                 self.pos = -1
 
-            def getfulllength():
+            def getfulllength(self):
                 """
                 Gets the length of the string including the length (=!) of the potential emptytail
                 """
-                if emptytail:
+                if self.emptytail:
                     return self._nblocks+1
                 else:
                     return self._nblocks
